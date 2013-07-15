e9002854d85ee52804dd4684c268e2a24e4b909f
Matthew Carter <m@ahungry.com>
2013-07-15 01:03:18 -0400

    Update to allow conditionals in checks

--- glyphs.lisp
+++ glyphs.lisp
@@ -12,17 +12,20 @@
 	 ,@(loop for arg in rest
 	      for iter from 0
 	      when (and (symbolp arg) (equal '→ (intern (string arg))))
-	      collect `((equal x ,(nth (- iter 1) rest))
-			,(nth (+ iter 1) rest)))))))
+	      collect `(,(if (consp (nth (- iter 1) rest))
+			     `,(nth (- iter 1) rest)
+			      `(equal x ,(nth (- iter 1) rest)))
+			,(nth (+ iter 1) rest)))))))))
 
 (defmacro λ (&rest rest)
   "Similar to lambda, requires using x as the default case"
   `(lambda (&optional x y z)
-     (let ((x (or x t)))
        (cond 
 	 ,@(loop for arg in rest
 	      for iter from 0
 	      when (and (symbolp arg) (equal '→ (intern (string arg))))
-	      collect `((equal x ,(nth (- iter 1) rest))
-			,(nth (+ iter 1) rest)))))))
+	      collect `(,(if (consp (nth (- iter 1) rest))
+			     `,(nth (- iter 1) rest)
+			      `(equal x ,(nth (- iter 1) rest)))
+			,(nth (+ iter 1) rest)))))))))
 
